5035ecb572f0ca1e158a5a2c68389e48648ad597
sssidkn <sonyadidkina0307@gmail.com>
2025-03-15 17:22:53 +0300

    fixed endpoints, added handlers architecture

--- backend/app-server/internal/server/router/endpoints.go
+++ backend/app-server/internal/server/router/endpoints.go
@@ -1,15 +1,29 @@
 package router
 
+// IndividualGroups
+
 const (
-	RegisterCompetition        = "/competitions/{id}/register"
-	RegisterUser               = "/users/register"
-	CreateCompetition          = "/competition"
-	CreateCup                  = "/cup"
-	CreateIndividualGroup      = "/individual_group"
-	CreateRangeGroup           = "/range_group"
-	CreateQualification        = "/qualification"
-	CreateQualificationRound   = "/qualification_round"
-	CreateQualificationSection = "/qualification_section"
-	CreateRange                = "/range"
-	CreateShot                 = "/shot"
+	CreateIndividualGroup = "api/individual_groups"
+)
+
+// Competitions
+const (
+	EndCompetition    = "api/competitions/{competition_id}/end"
+	CreateCompetition = "api/competition"
+	EditCompetition   = "api/competitions/{competition_id}"
+)
+
+// Cups
+const (
+	CreateCup = "api/cups"
+	Cup       = "api/cups/{cup_id}"
+)
+
+const (
+	CreateRangeGroup           = "api/range_group"
+	CreateQualification        = "api/qualification"
+	CreateQualificationRound   = "api/qualification_round"
+	CreateQualificationSection = "api/qualification_section"
+	CreateRange                = "api/range"
+	CreateShot                 = "api/shot"
 )
