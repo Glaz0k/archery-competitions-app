74802a745bfd395c63fd179864de681f8a52651f
TheOneFoxAgo <nikitad2005@gmail.com>
2025-04-13 13:20:51 +0300

    Made series card

--- frontend/mobile_app/lib/model/series.dart
+++
@@ -1,5 +0,0 @@
-class Series {
-  void add(int result) => results.add(result);
-  int get length => results.length;
-  List<int> results = [];
-}
--- frontend/mobile_app/lib/page/series_page.dart
+++ frontend/mobile_app/lib/page/series_page.dart
@@ -1,37 +1,78 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_app/model/series.dart';
 
-class SeriesPage extends StatefulWidget {
+import '../model/series_model.dart';
+
+class SeriesPage extends StatelessWidget {
   const SeriesPage({super.key});
 
   final String title = 'Серии';
 
   @override
-  State<SeriesPage> createState() => _SeriesPageState();
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
 }
 
-class _SeriesPageState extends State<SeriesPage> {
-  final List<Series> results = [Series()];
+class SeriesCard extends StatefulWidget {
+  final Widget name;
+  final Series series;
+  final bool active;
+  final void Function() onAdd;
+  final void Function(int, int) onChange;
+
+  const SeriesCard({
+    super.key,
+    required this.name,
+    required this.series,
+    required this.onAdd,
+    required this.onChange,
+    required this.active,
+  });
+
+  @override
+  State<StatefulWidget> createState() {
+    return _SeriesCardState();
+  }
+}
+
+class _SeriesCardState extends State<SeriesCard> {
+  int? _selected;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Placeholder(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _addNewSeries,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+    List<Widget> body = [
+      widget.name,
+      ...widget.series.scores.indexed.map((r) {
+        var (index, score) = r;
+        var isSelected = index == _selected;
+        var text = Text('$score');
+        if (isSelected) {
+          return FilledButton(onPressed: () => _selectScore(null), child: text);
+        } else {
+          return FilledButton.tonal(
+            onPressed: () => _selectScore(index),
+            child: text,
+          );
+        }
+      }),
+      FloatingActionButton(onPressed: widget.onAdd, child: Icon(Icons.add)),
+    ];
+    var selected = _selected;
+    return Column(
+      children: [
+        ListView(scrollDirection: Axis.horizontal, children: body),
+        AnimatedContainer(duration: Duration(microseconds: 200),
+          child: selected != null? Slider(
+            value: widget.series.scores[selected].toDouble(),
+            divisions: 10,
+            onChanged: (score) => widget.onChange(selected, score.round()),
+          ) : SizedBox.shrink(),
+        )
+      ],
     );
   }
-  void _addNewSeries() => setState(() {
-    if (results.last.length > 5) {
-      results.add(Series());
-    } else {
-     // int? newResult = _showNumberPicker();
-    }
+
+  void _selectScore(int? index) => setState(() {
+    _selected = index;
   });
 }
