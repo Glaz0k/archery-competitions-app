d4f9379ddfd0a46586a8d484a6e2a3b89a481399
Glaz0k <nikitakravcenko892@gmail.com>
2025-04-16 17:28:04 +0300

    Set up prod build, optimized chunks

--- frontend/web-app/vite.config.js
+++ frontend/web-app/vite.config.js
@@ -1,7 +1,37 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default {
   plugins: [react()],
-});
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom", "react-router"],
+
+          data: ["@tanstack/react-query"],
+
+          mantine: [
+            "@mantine/core",
+            "@mantine/dates",
+            "@mantine/form",
+            "@mantine/hooks",
+            "@tabler/core",
+            "@tabler/icons-react",
+            "postcss-preset-mantine",
+          ],
+
+          dates: ["date-fns", "date-fns-tz", "dayjs"],
+
+          devtools: [
+            "@eslint/js",
+            "eslint",
+            "prettier",
+            "@ianvs/prettier-plugin-sort-imports",
+            "prettier-plugin-organize-imports",
+          ],
+        },
+      },
+    },
+  },
+};
