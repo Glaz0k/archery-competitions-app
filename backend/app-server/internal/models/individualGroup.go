4c8fd6f8d940c33c16a4a11be6a7bdc571d6113a
sssidkn <sonyadidkina0307@gmail.com>
2025-05-02 16:54:39 +0300

    fix identity

--- backend/app-server/internal/models/individualGroup.go
+++ backend/app-server/internal/models/individualGroup.go
@@ -4,8 +4,8 @@ type IndividualGroup struct {
 	ID            int     `json:"id"`
 	CompetitionID int     `json:"competition_id"`
 	Bow           *string `json:"bow"`
-	Identity      string  `json:"identity"`
-	State         string  `json:"state"`
+	Identity      *string `json:"identity"`
+	State         *string `json:"state"`
 }
 
 type FinalGrid struct {
