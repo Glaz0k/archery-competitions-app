ee7caa9c48d99ce9f333b99310c8e3c8a87aa707
TheOneFoxAgo <nikitad2005@gmail.com>
2025-05-04 18:39:02 +0300

    Adjusted readme and exceptions.

--- frontend/mobile_app/lib/api/exceptions.dart
+++ frontend/mobile_app/lib/api/exceptions.dart
@@ -1,24 +1,28 @@
-import 'dart:io';
-
 import 'package:mobile_app/api/common.dart';
 
-class NotFoundException extends HttpException {
+abstract class OnionException implements Exception {
+  final String message;
+
+  OnionException(this.message);
+}
+
+class NotFoundException extends OnionException {
   NotFoundException(super.message);
 }
 
-class InvalidParametersException extends HttpException {
+class InvalidParametersException extends OnionException {
   InvalidParametersException(super.message);
 }
 
-class AlreadyExistException extends HttpException {
+class AlreadyExistException extends OnionException {
   AlreadyExistException(super.message);
 }
 
-class BadActionException extends HttpException {
+class BadActionException extends OnionException {
   BadActionException(super.message);
 }
 
-class InvalidScoreException extends HttpException {
+class InvalidScoreException extends OnionException {
   final int shotOrdinal;
   final RangeType rangeType;
   InvalidScoreException(super.message, this.shotOrdinal, this.rangeType);
