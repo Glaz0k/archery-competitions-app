119419d336c307291419bcf66eeb4f0b12633d37
Glaz0k <nikitakravcenko892@gmail.com>
2025-05-07 17:23:43 +0300

    Restore crucial files

---
+++ frontend/web-app/src/features/group-sections/index.ts
@@ -0,0 +1,6 @@
+export { CompetitorsSection } from "./components/CompetitorsSection";
+export { QualificationSection } from "./components/QualificationSection";
+
+export { GroupSectionsContext } from "./context/GroupSectionsContext";
+export type { GroupSections } from "./context/GroupSectionsContext";
+export { useGroupSections } from "./context/useGroupSections";
