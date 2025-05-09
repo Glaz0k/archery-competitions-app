7f690bae10f460761411e79697598b8839f42bfc
TheOneFoxAgo <nikitad2005@gmail.com>
2025-05-09 12:11:16 +0300

    Implemented initial sketch of hub page of individual group.

--- frontend/mobile_app/lib/page/widgets/onion_bar.dart
+++ frontend/mobile_app/lib/page/widgets/onion_bar.dart
@@ -2,12 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:mobile_app/page/profile_page.dart';
 
 class OnionBar extends AppBar {
-  OnionBar(String title, BuildContext context, {super.key}) : super(
+  OnionBar(String title, BuildContext context, {super.key, super.bottom})
+      : super(
     title: Text(title),
     backgroundColor: Theme.of(context).colorScheme.inversePrimary,
     actions: [_ProfileButton()]
   );
-  OnionBar.withoutProfile(String title, BuildContext context, {super.key}) : super(
+
+  OnionBar.withoutProfile(String title, BuildContext context,
+      {super.key, super.bottom}) : super(
       title: Text(title),
       backgroundColor: Theme.of(context).colorScheme.inversePrimary
   );
