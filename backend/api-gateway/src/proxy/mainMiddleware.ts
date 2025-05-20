b3844f6e6a74d32dd187ed98f464a82320bc2774
Glaz0k <nikitakravcenko892@gmail.com>
2025-05-20 16:40:26 +0300

    Fixed main CORS

--- backend/api-gateway/src/proxy/mainMiddleware.ts
+++ backend/api-gateway/src/proxy/mainMiddleware.ts
@@ -5,4 +5,8 @@ const { MAIN_URL } = process.env;
 export const mainMiddleware = createProxyMiddleware({
   target: MAIN_URL,
   changeOrigin: true,
+  onProxyRes: (proxyRes) => {
+    delete proxyRes.headers["access-control-allow-origin"];
+    delete proxyRes.headers["access-control-allow-credentials"];
+  },
 });
